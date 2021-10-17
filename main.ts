80207dda091454c9cd4a62512a043570fcecbe34
Thimo <thimodehaan@gmail.com>
2021-10-17 15:42:45 +0200

    workin first slash command

--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ import DiscordJS, { Client, Intents } from 'discord.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
 const client = new DiscordJS.Client({
     intents: [
         Intents.FLAGS.GUILDS,
@@ -10,13 +12,35 @@ const client = new DiscordJS.Client({
 })
 
 client.on('ready', () =>{
-    console.log('Ready')
+    console.log(`Logged in as ${client.user.tag}`)
+
+    const guildId = '457250278912032790'
+    const guild = client.guilds.cache.get(guildId)
+    let commands
+
+    if (guild) {
+        commands = guild.commands
+    } else {
+        commands = client.application?.commands
+    }
+
+    commands?.create({
+        name: 'fish',
+        description: 'Replies with < > <'
+    })
+
 })
 
-client.on('messageCreate', (message) =>{
-    if (message.content === 'ping') {
-        message.reply({
-            content: 'pong',
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) {
+        return
+}
+const {commandName, option} = interaction
+
+    if (commandName === 'fish'){
+        interaction.reply({
+            content: '< > <',
+            ephemeral: false
         })
     }
 })
