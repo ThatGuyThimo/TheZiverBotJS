1754d0583c5221761bf23302f4e2ae81d2f8af16
Thimo <thimodehaan@gmail.com>
2024-01-16 22:40:15 +0100

    chnaged api response

--- API/index.js
+++ API/index.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => {
   let data = JSON.parse(fs.readFileSync("../Data/members.json"))
-  res.send(data.Zivergroup)
+  res.send(`${data.Zivergroup}`).status(200)
 })
 
 // app.use('/cars', carRoutes)
